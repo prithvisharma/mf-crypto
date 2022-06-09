7524dc9e08fee46652c3292d677b6207d696b4df
Prithvi Sharma <53996107+prithvisharma@users.noreply.github.com>
2022-06-10 01:46:22 +0530

    update call DatabaseInitializer.load(), after spring initialisation

--- src/main/java/com/mf/crypto/MfCryptoApplication.java
+++ src/main/java/com/mf/crypto/MfCryptoApplication.java
@@ -3,11 +3,16 @@ package com.mf.crypto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.mf.crypto.common.SpringApplicationContext;
+import com.mf.crypto.database.common.DatabaseInitializer;
+import com.mf.crypto.exception.ServiceException;
+
 @SpringBootApplication
 public class MfCryptoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MfCryptoApplication.class, args);
-	}
+    public static void main(String[] args) throws ServiceException {
+        SpringApplication.run(MfCryptoApplication.class, args);
+        SpringApplicationContext.getBean(DatabaseInitializer.class).load();
+    }
 
 }
